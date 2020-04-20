96d8f54989c3fa2b10c44b2ab68cad9b48da3f70
Shawn5141 <s19911111@gmail.com>
2020-04-19 17:39:34 -0700

    template for part2

---
+++ src/Lab1/Part2.java
@@ -0,0 +1,31 @@
+package Lab1;
+
+import java.io.IOException;
+
+/*
+The server should start listening on port 12235 (Do not run your server on attu2 on the same port as our server is running here).
+The server should handle multiple clients at a time. This can be done using threading. Spawn a seperate thread for every new client and kill the thread when the client finishes.
+The server should verify the header of every packet received and close any open sockets to the client and/or fail to respond to the client if:
+unexpected number of buffers have been received
+unexpected payload, or length of packet or length of packet payload has been received
+the server does not receive any packets from the client for 3 seconds
+the server does not receive the correct secret
+The Server should respond to the client in four stages.In each stage, the server should randomly generate a secret to be sent to the client.
+ */
+public class Part2 {
+    private void stageA(){
+
+    }
+    private void stageB(){
+
+    }
+    private void stageC(){
+
+    }
+    private void stageD(){
+
+    }
+    public static void main(String[] args) throws IOException{
+
+    }
+}
