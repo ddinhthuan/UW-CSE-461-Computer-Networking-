af6bd62820ee821832d86c9312cc744234bdecea
Shawn5141 <s19911111@gmail.com>
2020-04-20 16:33:25 -0700

    finish part c

--- src/Lab1/ClientHandler.java
+++ src/Lab1/ClientHandler.java
@@ -2,10 +2,7 @@ package Lab1;
 
 
 import java.io.*;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetSocketAddress;
-import java.net.Socket;
+import java.net.*;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Date;
@@ -13,13 +10,13 @@ import java.util.Random;
 public class ClientHandler extends Thread{
     //copy from lab1
     private static DatagramSocket udpSocket = null;
-    private static Socket tcpSocket = null;
+    private static ServerSocket tcpSocket = null;
 
-
-//    final  socket;
-      int psecretA =0;
-      int psecretB =Integer.MAX_VALUE;
-      int psecretC =Integer.MAX_VALUE;
+    private  int psecretA =0;
+    private  int psecretB =Integer.MAX_VALUE;
+    private  int psecretC =Integer.MAX_VALUE;
+    private int studentID;
+    private int tcp_port;
     private static final int TIMEOUT = 1000;
     private static final String HOSTNAME = "localhost";
     private static InputStream in =null;
@@ -52,15 +49,16 @@ public class ClientHandler extends Thread{
                 int clientPsecret=receivedBuf.getInt(4);
                 short step=receivedBuf.getShort(8);
                 short studentID=receivedBuf.getShort(10);
+                //TODO Check 4 alginment
                 if(step!=1){
                     if(udpSocket!=null&& udpSocket.isConnected())closeUDPSocket();
-                    if(tcpSocket!=null&& tcpSocket.isConnected())closeTCPSocket();
+                    if(tcpSocket!=null&& !tcpSocket.isClosed())closeTCPSocket();
                     break;
                 }
                 if(clientPsecret==psecretA ) {
                     stageB();
                 }else if(clientPsecret==psecretB ){
-                    stageC();
+                    stageC(psecretB);
                 }else if(clientPsecret==psecretC){
                     stageD();
                 }
@@ -80,7 +78,39 @@ public class ClientHandler extends Thread{
     private void stageB(){
 
     }
-    private void stageC(){
+    private void stageC(int clientPsecret){
+        ByteBuffer resp=null;
+
+
+        try{
+
+            initializeTCPSocket(tcp_port);
+            Socket clientSocket = tcpSocket.accept();
+
+
+            Random rand = new Random();
+            int num2 = rand.nextInt(100);
+            int len2 = rand.nextInt(100);
+            psecretC = rand.nextInt(1000);
+
+            //Assign to return val
+            ByteBuffer responsePacket =ByteBuffer.allocate(12+14);
+            header head =new header(len2,clientPsecret, 2,studentID);
+            responsePacket.put(head.byteBuffer.array());
+            responsePacket.putInt(num2);
+
+            responsePacket.putInt(len2);
+            responsePacket.putInt(psecretC);
+            responsePacket.putChar('c');
+            PrintWriter pr =new PrintWriter(clientSocket.getOutputStream());
+            //https://docs.oracle.com/javase/8/docs/api/java/io/PrintWriter.html
+            pr.println(Arrays.toString(responsePacket.array()));
+            pr.flush();
+            System.out.println("sent to client with "+Arrays.toString(responsePacket.array()));
+        }catch (IOException e){
+            System.err.println("connection failed");
+        }
+
 
     }
     private void stageD(){
@@ -94,9 +124,7 @@ public class ClientHandler extends Thread{
 
     private static void initializeTCPSocket(int tcp_port) throws IOException {
         try {
-            tcpSocket = new Socket();
-            InetSocketAddress address = new InetSocketAddress(HOSTNAME, tcp_port);
-            tcpSocket.connect(address, TIMEOUT);
+            tcpSocket = new ServerSocket(tcp_port);
 
         } catch (IOException e){
             System.err.println("Could not connect");
