f3c475d271fe84f3efcea964c74871a495afdcf3
Shawn5141 <s19911111@gmail.com>
2020-05-26 21:32:23 -0700

    use proxy_port=1234 and can get command from client

--- src/Lab3/ProxyServer.java
+++ src/Lab3/ProxyServer.java
@@ -11,12 +11,12 @@ public class ProxyServer {
     private ServerSocket tcpSocket =null;
 
     public static void main(String[] args)throws IOException{
-
+        ProxyServer proxyServer = new ProxyServer(1234);
 
     }
-    public ProxyServer(){
+    public ProxyServer(int proxy_port){
         //Listening to port 1234
-        InitialTCPSocket(1234);
+        InitialTCPSocket(proxy_port);
         Listener listener =new Listener();
         Thread thread = new Thread(listener);
         thread.start();
@@ -24,9 +24,9 @@ public class ProxyServer {
     }
 
 
-    private void InitialTCPSocket(int tcp_port){
+    private void InitialTCPSocket(int proxy_port){
         try {
-            tcpSocket = new ServerSocket(tcp_port);
+            tcpSocket = new ServerSocket(proxy_port);
             System.out.println("Server started");
             System.out.println("Waiting for a client...");
 
@@ -39,19 +39,23 @@ public class ProxyServer {
     class Listener implements Runnable {
         @Override
         public void run() {
+
             try{
 
                 Socket proxySocket = tcpSocket.accept();
+                //Parse first line
+
+                System.out.println("client connected: " + proxySocket.isConnected());
+                //Coming from origin server
+                //Coming from client
+                //1. Connect command
+                //2. Non connect Command
             }catch(IOException e){
 
             }
-            //Coming from origin server
-
-            //Coming from client
-                //1. Connect command
 
 
-                //2. Non connect Command
+
 
 
 
