67aba60e34b0af2869d66808fee8ff8e6f995bb8
Jazmine Redmond <jazjred@cs.washington.edu>
2020-05-28 03:53:08 -0700

    Need to send GET to browser

--- src/Lab3/HttpHeader.java
+++ src/Lab3/HttpHeader.java
@@ -19,12 +19,23 @@ public class HttpHeader {
         return tmp.substring(0, idx2);
     }
 
-    public String modifyKeepAlive() {
+    public HttpHeader transformRequestHeader() {
         // filter the request and response streams,
         // removing any Connection: keep-alive, inserting a Connection: close, and
         // converting any Proxy-connection: keep-alive to Proxy-connection: close.
 
-        return request.replace("keep-alive", "close");
+        // lower the HTTP version of the request to HTTP 1.0
+         HttpHeader tmp = new HttpHeader(request
+                 .replace("/1.1", "/1.0")
+                 .replace("keep-alive", "close"));
+
+       //  System.out.println("MODIFIED HEADER\n" + tmp.getRequest());
+        return tmp;
+    }
+
+    public boolean isConnect(){
+        //blank space necessary or else "CONNECTion"
+        return request.toLowerCase().contains("connect ");
     }
 
     public String getRequest() {
--- src/Lab3/ProxyServer.java
+++ src/Lab3/ProxyServer.java
@@ -1,7 +1,5 @@
 package Lab3;
 
-import Test.Server;
-
 import java.io.*;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
@@ -9,13 +7,11 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.Arrays;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
 public class ProxyServer {
-//    private ServerSocket tcpSocket =null;
 
     public static void main(String[] args){
         //TODO uncomment when ready
@@ -30,7 +26,6 @@ public class ProxyServer {
             throw new IllegalArgumentException("Invalid port number");
         }
 
-        ProxyServer proxyServer = new ProxyServer(port);
         */
 
         try (ServerSocket tcpSocket = new ServerSocket(port)) {
@@ -64,14 +59,30 @@ public class ProxyServer {
                     Calendar.LONG, Locale.getDefault()) + " " + time + " - ");
         }
 
+        private int parsePortNum(HttpHeader request){
+            //parse first line and host line for port num
+            // if no port specified - use 443 if https:// or 80 otherwise
+
+            String hostLine = request.getHostLine();
+            if(hostLine.substring(5, hostLine.length()-1).contains(":")) //TODO test edge cases
+                return Integer.parseInt(hostLine);
+
+            // look for one in te request line
+//            String firstLine = request.getStartLine();
+//            if(firstLine.contains(":")) //TODO fix logic
+//                return Integer.parseInt(firstLine);
+
+            if(hostLine.toLowerCase().contains("https://"))
+                return 443;
+
+            return 80;
+        }
+
         @Override
         public void run() {
-            //Parse first line
-
             //Implement Handler
             //Coming from origin server - header from user request
             // e.g. GET hostname - parse out hostname then send to host
-            //modify keep alive flag - set to false if GET request
             //e.g. CONNECT request --> set up back and forth request
 
             //Client sends request to web server
@@ -82,28 +93,20 @@ public class ProxyServer {
                 byte[] reply_bytes = new byte[4096];
 
                 InputStream inFromClient = connection.getInputStream();
-                OutputStream out = connection.getOutputStream();
+                OutputStream outToClient = connection.getOutputStream();
 
-               /* //connect socket to server
-                try{
-                    server = new Socket(connection.getInetAddress(), 1234); //todo fix hardcoded
-                } catch (IOException e){
-                    PrintWriter out = new PrintWriter(new OutputStreamWriter(outToClient));
-                    out.flush();
-                    throw new RuntimeException(e);
-                }
 
-                */
 //                InputStream inFromServer = server.getInputStream();
              //   OutputStream outToServer = server.getOutputStream();
 
 
                 //Request asks server to retrieve resource - identified by URI
                 int bytes_read;
+                HttpHeader request = null;
                 while((bytes_read = inFromClient.read(request_bytes)) != -1){
-                    out.write(request_bytes, 0, bytes_read);
+                    outToClient.write(request_bytes, 0, bytes_read);
                     String requestString = new String(request_bytes, StandardCharsets.UTF_8);
-                    HttpHeader request = new HttpHeader(requestString);
+                    request = new HttpHeader(requestString);
                     //System.out.println("to server-->\n" + request.getRequest()); //for debugging
 
                     //print out first line of each HTTP request
@@ -112,24 +115,56 @@ public class ProxyServer {
                     // (which additionally includes the HTTP version) if that's easier
                     printDateStamp();
                     System.out.println(" >>> " + request.getStartLine());
-                    //    System.out.println(request.getHostLine()); //for debugging
+                    outToClient.flush();
+                }
+                assert(request != null);
 
-                    out.flush();
+                //For non-CONNECT HTTP requests - edit the HTTP request header, send it
+                // and any payload the request might carry to the origin server,
+                // and then slightly edit the HTTP response header and
+                // send it and any response payload back to the browser.
+                if(!request.isConnect()){
+
+                    System.out.println(request.getHostLine()); //for debugging
+                   int port = parsePortNum(request);
+                    System.out.println("Port: " + port); //debugging
+
+                    request = request.transformRequestHeader();
+
+                    //todo open a connection and send to browser
+
+                } else { //is connect
+                    //For CONNECT HTTP requests - establish TCP connection to the server named
+                    // in the request, send an HTTP success response to the browser,
+                    // then simply pass through any data sent by the browser
+                    // or the remote server to the other end of the communication.
+                    //TODO
+
+                    System.err.println("Not yet implemented");
+                    /*
+                    try {
+                        server = new Socket(connection.getInetAddress(), 1234); //todo fix hardcoded
+
+                        System.out.println("connect to server");
+                        PrintWriter out = new PrintWriter(new OutputStreamWriter(outToClient));
+                        out.write("HTTP/1.1 200 OK/\r\n\r\n");
+                        out.flush();
+                    } catch (IOException e) {
+                        PrintWriter out = new PrintWriter(new OutputStreamWriter(outToClient));
+                        out.flush();
+                        throw new RuntimeException(e);
+                    }
+
+                     */
+
                 }
 
 
-//Server processes request - sends response
+
+
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-            //Coming from client
-            //1. Connect command
-            //2. Non connect Command
-
-
         }
-
     }
-
 }
