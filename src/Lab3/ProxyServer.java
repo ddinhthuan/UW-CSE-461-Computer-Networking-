63c5b368cd31bc9acd72ba9680cc217364808e3f
Jazmine Redmond <jazjred@cs.washington.edu>
2020-05-28 02:38:11 -0700

    Fixed multithreading:

--- src/Lab3/ProxyServer.java
+++ src/Lab3/ProxyServer.java
@@ -15,10 +15,11 @@ import java.util.Date;
 import java.util.Locale;
 
 public class ProxyServer {
+//    private ServerSocket tcpSocket =null;
 
-    private static ServerSocket tcpSocket = null;
-    public static void main(String[] args) {
+    public static void main(String[] args){
         //TODO uncomment when ready
+        int port = 1234;
         /*
         if(args.length != 2){
             throw new IllegalArgumentException("insufficient arguments");
@@ -31,35 +32,30 @@ public class ProxyServer {
 
         ProxyServer proxyServer = new ProxyServer(port);
         */
-        int port = 1234;
-        try {
-            tcpSocket = new ServerSocket(port);
-            //printDateStamp();
-            System.out.println("Proxy listening on " + tcpSocket.getLocalSocketAddress()); //todo fix
 
-            //System.out.println("Server started");
-            //System.out.println("Waiting for a client...");
-            while(true){
-                //ThreadProxy threadProxy = new ThreadProxy(tcpSocket.accept(),port);
-                Listener listener = new Listener(tcpSocket.accept());
-                System.out.println("connected create thread");
-                Thread thread = new Thread(listener);
-                thread.start();
+        try (ServerSocket tcpSocket = new ServerSocket(port)) {
+            while (true) {
+                try {
+                    Socket connection = tcpSocket.accept();
+                    Thread task = new Proxy(connection);
+                    task.start();
+                } catch (IOException ex) {
+                }
             }
-
-            } catch (IOException e) {
-            System.err.println("Could not connect");
-            System.err.println(e);
+        }catch (IOException ex){
+            System.err.println("Couldn't start server");
         }
     }
 
-}
 
-    class Listener implements Runnable {
-        Socket proxySocket=null;
-        Listener(Socket proxySocket){
-            this.proxySocket = proxySocket;
+    private static class Proxy extends Thread {
+        private Socket connection;
+        Proxy (Socket connection){
+            this.connection = connection;
+            printDateStamp();
+            System.out.println("Proxy listening on " + connection.getLocalSocketAddress()); //todo fix
         }
+
         private void printDateStamp() {
             Calendar cal = Calendar.getInstance();
             DateFormat df = new SimpleDateFormat("hh:mm:ss");
@@ -85,70 +81,55 @@ public class ProxyServer {
                 byte[] request_bytes = new byte[1024];
                 byte[] reply_bytes = new byte[4096];
 
-                InputStream inFromClient = proxySocket.getInputStream();
-                OutputStream outToClient = proxySocket.getOutputStream();
+                InputStream inFromClient = connection.getInputStream();
+                OutputStream out = connection.getOutputStream();
 
-                BufferedReader reader = new BufferedReader(new InputStreamReader(inFromClient));
-                String line= reader.readLine();
-                reader.mark(0);
-                reader.reset();
-                System.out.println("=========================="+line+"========================");
-                //connect socket to server
-                if(line.contains("CONNECT")) {
-                    try {
-                        server = new Socket(proxySocket.getInetAddress(), 1234); //todo fix hardcoded
-
-                        System.out.println("connect to server");
-                        PrintWriter out = new PrintWriter(new OutputStreamWriter(outToClient));
-                        out.write("HTTP/1.1 200 OK/\r\n\r\n");
-                        out.flush();
-                    } catch (IOException e) {
-                        PrintWriter out = new PrintWriter(new OutputStreamWriter(outToClient));
-                        out.flush();
-                        throw new RuntimeException(e);
-                    }
+               /* //connect socket to server
+                try{
+                    server = new Socket(connection.getInetAddress(), 1234); //todo fix hardcoded
+                } catch (IOException e){
+                    PrintWriter out = new PrintWriter(new OutputStreamWriter(outToClient));
+                    out.flush();
+                    throw new RuntimeException(e);
                 }
-                    InputStream inFromServer = server.getInputStream();
-                    OutputStream outToServer = server.getOutputStream();
+
+                */
+//                InputStream inFromServer = server.getInputStream();
+             //   OutputStream outToServer = server.getOutputStream();
 
 
-                    //Request asks server to retrieve resource - identified by URI
-                    int bytes_read;
-                    while ((bytes_read = inFromClient.read(request_bytes)) != -1) {
-                        outToServer.write(request_bytes, 0, bytes_read);
-                        String requestString = new String(request_bytes, StandardCharsets.UTF_8);
-                        HttpHeader request = new HttpHeader(requestString);
-                        //        System.out.println("to server-->\n" + request); //for debugging
+                //Request asks server to retrieve resource - identified by URI
+                int bytes_read;
+                while((bytes_read = inFromClient.read(request_bytes)) != -1){
+                    out.write(request_bytes, 0, bytes_read);
+                    String requestString = new String(request_bytes, StandardCharsets.UTF_8);
+                    HttpHeader request = new HttpHeader(requestString);
+                    //System.out.println("to server-->\n" + request.getRequest()); //for debugging
 
-                        //print out first line of each HTTP request
-                        // must print at least the HTTP method and URI given on the request line,
-                        // but you can also print the entire request line
-                        // (which additionally includes the HTTP version) if that's easier
-                        printDateStamp();
-                        System.out.println(" >>> " + request.getStartLine());
+                    //print out first line of each HTTP request
+                    // must print at least the HTTP method and URI given on the request line,
+                    // but you can also print the entire request line
+                    // (which additionally includes the HTTP version) if that's easier
+                    printDateStamp();
+                    System.out.println(" >>> " + request.getStartLine());
+                    //    System.out.println(request.getHostLine()); //for debugging
 
-                        //    System.out.println(request.getHostLine()); //for debugging
+                    out.flush();
+                }
 
-                        outToServer.flush();
-                    }
 
 //Server processes request - sends response
             } catch (IOException e) {
                 e.printStackTrace();
             }
 
-
-
-
-
             //Coming from client
-                    //1. Connect command
-                    //2. Non connect Command
+            //1. Connect command
+            //2. Non connect Command
 
 
         }
 
     }
 
-
-
+}
